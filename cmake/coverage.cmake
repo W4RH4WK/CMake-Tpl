a27eaa5413d40dba967cf441de40affd01c945c1
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-05-15 15:49:56 +0200

    Add coverage

---
+++ cmake/coverage.cmake
@@ -0,0 +1,46 @@
+find_program(LCOV lcov)
+find_program(GENHTML genhtml)
+
+if(NOT LCOV)
+	message(FATAL_ERROR "lcov not found")
+endif()
+
+if(NOT GENHTML)
+	message(FATAL_ERROR "genhtml not found")
+endif()
+
+set(_c_compiler_ids GNU Clang AppleClang)
+if(CMAKE_C_COMPILE_ID AND NOT CMAKE_C_COMPILE_ID IN_LIST _c_compiler_ids)
+	message(FATAL_ERROR "compiler does not support coverage")
+else()
+	set(CMAKE_C_FLAGS "--coverage ${CMAKE_C_FLAGS}")
+endif()
+
+set(_cxx_compiler_ids GNU Clang AppleClang)
+if(CMAKE_CXX_COMPILER_ID AND NOT CMAKE_CXX_COMPILER_ID IN_LIST _cxx_compiler_ids)
+	message(FATAL_ERROR "compiler does not support coverage")
+else()
+	set(CMAKE_CXX_FLAGS "--coverage ${CMAKE_CXX_FLAGS}")
+endif()
+
+set(CMAKE_EXE_LINKER_FLAGS    "--coverage ${CMAKE_EXE_LINKER_FLAGS}")
+set(CMAKE_SHARED_LINKER_FLAGS "--coverage ${CMAKE_SHARED_LINKER_FLAGS}")
+
+set(_lcov_args
+	--base-directory ${PROJECT_BINARY_DIR}
+	--directory ${PROJECT_BINARY_DIR}
+	--quiet
+)
+
+set(_genhtml_args --title ${PROJECT_NAME} --quiet)
+
+add_custom_target(coverage
+	COMMAND ${LCOV} ${_lcov_args} --zerocounters
+	COMMAND ${CMAKE_BUILD_TOOL} test
+	COMMAND ${LCOV} ${_lcov_args} --capture -o coverage.info
+	COMMAND ${LCOV} ${_lcov_args} --remove coverage.info "/usr*"  -o coverage.info
+	COMMAND ${LCOV} ${_lcov_args} --remove coverage.info "boost*" -o coverage.info
+	COMMAND ${LCOV} ${_lcov_args} --remove coverage.info "c++*"   -o coverage.info
+	COMMAND ${LCOV} ${_lcov_args} --remove coverage.info "gtest*" -o coverage.info
+	COMMAND ${GENHTML} ${_genhtml_args} coverage.info -o coverage
+)
