46e87c104a7a74b5a8b32f717cd4a378112f964e
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-05-29 15:11:58 +0200

    Introduce ENABLE_DOXYGEN and ENABLE_COVERAGE flags

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,8 +1,16 @@
-find_package(Doxygen REQUIRED)
+option(ENABLE_DOXYGEN "Add target to build doxygen output" ON)
 
-configure_file(${PROJECT_SOURCE_DIR}/.doxyfile ${PROJECT_BINARY_DIR}/Doxyfile @ONLY)
+if(ENABLE_DOXYGEN)
+    find_package(Doxygen)
 
-add_custom_target(doxygen
-	COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/Doxyfile
-	SOURCES ${PROJECT_SOURCE_DIR}/.doxyfile
-)
+    if(DOXYGEN_FOUND)
+        configure_file(${PROJECT_SOURCE_DIR}/.doxyfile ${PROJECT_BINARY_DIR}/Doxyfile @ONLY)
+
+        add_custom_target(doxygen
+	        COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/Doxyfile
+	        SOURCES ${PROJECT_SOURCE_DIR}/.doxyfile
+        )
+    else()
+        message(WARNING "Doxygen not found.")
+    endif()
+endif()
