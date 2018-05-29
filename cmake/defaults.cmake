f9d653cc7618ed6218054c32c754de55a35d21b9
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-05-29 15:16:37 +0200

    Split base defaults from project related defaults

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -3,21 +3,3 @@ option(AUTO_DOWNLOAD "Automatically download build dependencies" OFF)
 if(NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "CMake Build Type" FORCE)
 endif()
-
-set(CMAKE_C_STANDARD 11)
-set(CMAKE_C_STANDARD_REQUIRED ON)
-set(CMAKE_C_EXTENSIONS OFF)
-
-if(CMAKE_C_COMPILE_ID MATCHES "GNU|Clang|AppleClang")
-	set(CMAKE_C_FLAGS "-Wall -Wextra ${CMAKE_C_FLAGS}")
-endif()
-
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang|AppleClang")
-	set(CMAKE_CXX_FLAGS "-Wall -Wextra ${CMAKE_CXX_FLAGS}")
-endif()
-
-enable_testing()
---
+++ cmake/project_defaults.cmake
@@ -0,0 +1,21 @@
+option(ENABLE_TESTING "Enable tests" ON)
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS OFF)
+
+if(CMAKE_C_COMPILE_ID MATCHES "GNU|Clang|AppleClang")
+	set(CMAKE_C_FLAGS "-Wall -Wextra ${CMAKE_C_FLAGS}")
+endif()
+
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang|AppleClang")
+	set(CMAKE_CXX_FLAGS "-Wall -Wextra ${CMAKE_CXX_FLAGS}")
+endif()
+
+if(ENABLE_TESTING)
+    enable_testing()
+endif()
