13391da3a7c7fca3f1380c7173ae5dc5b8bfe7ba
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-05-29 15:23:32 +0200

    Introduce ENABLE_TESTING and ENABLE_BENCHMARKING flags

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -1,4 +1,6 @@
 option(AUTO_DOWNLOAD "Automatically download build dependencies" OFF)
+option(ENABLE_TESTING "Enable tests" ON)
+option(ENABLE_BENCHMARKING "Enable benchmarks" ON)
 
 if(NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "CMake Build Type" FORCE)
--- cmake/dependencies/googletest.cmake
+++ cmake/dependencies/googletest.cmake
@@ -1,22 +1,24 @@
-set(INSTALL_GTEST OFF CACHE BOOL "Enable installation of googletest." FORCE)
-set(INSTALL_GMOCK OFF CACHE BOOL "Enable installation of googlemock." FORCE)
+if(ENABLE_TESTING)
+    set(INSTALL_GTEST OFF CACHE BOOL "Enable installation of googletest." FORCE)
+    set(INSTALL_GMOCK OFF CACHE BOOL "Enable installation of googlemock." FORCE)
 
-if(EXISTS ${PROJECT_SOURCE_DIR}/vendor/googletest)
-	add_subdirectory(${PROJECT_SOURCE_DIR}/vendor/googletest)
-elseif(NOT AUTO_DOWNLOAD)
-	message(FATAL_ERROR "GoogleTest not found, consider using -DAUTO_DOWNLOAD=ON")
-else()
-	include(DownloadProject)
-	download_project(
-		PROJ googletest
-		URL https://github.com/google/googletest/archive/release-1.8.0.zip
-		DOWNLOAD_DIR ${PROJECT_BINARY_DIR}/vendor/googletest-download
-		SOURCE_DIR ${PROJECT_BINARY_DIR}/vendor/googletest-src
-		BINARY_DIR ${PROJECT_BINARY_DIR}/vendor/googletest
-		QUIET
-	)
-	add_subdirectory(
-		${PROJECT_BINARY_DIR}/vendor/googletest-src
-		${PROJECT_BINARY_DIR}/vendor/googletest
-	)
+    if(EXISTS ${PROJECT_SOURCE_DIR}/vendor/googletest)
+	    add_subdirectory(${PROJECT_SOURCE_DIR}/vendor/googletest)
+    elseif(NOT AUTO_DOWNLOAD)
+	    message(FATAL_ERROR "GoogleTest not found, consider using -DAUTO_DOWNLOAD=ON")
+    else()
+	    include(DownloadProject)
+	    download_project(
+		    PROJ googletest
+		    URL https://github.com/google/googletest/archive/release-1.8.0.zip
+		    DOWNLOAD_DIR ${PROJECT_BINARY_DIR}/vendor/googletest-download
+		    SOURCE_DIR ${PROJECT_BINARY_DIR}/vendor/googletest-src
+		    BINARY_DIR ${PROJECT_BINARY_DIR}/vendor/googletest
+		    QUIET
+	    )
+	    add_subdirectory(
+		    ${PROJECT_BINARY_DIR}/vendor/googletest-src
+		    ${PROJECT_BINARY_DIR}/vendor/googletest
+	    )
+    endif()
 endif()
